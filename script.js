20faff203aa8375550f2a896a250c4365280f22e
gamanedy144 <gamanedy@yahoo.com>
2021-07-22 19:08:52 +0300

    Add GUI for rock-paper-scissors game

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const resultContainer = document.querySelector('#results-container');
+const resultText = document.querySelector('#result-text');
+const resultChoices = document.querySelector('#result-choice');
+const resultScore = document.querySelector('#result-score');
+const resultEnd = document.querySelector('#result-end');
+let  playerScore = 0;
+let computerScore = 0;
+
 function computerPlay(){
     let choice = Math.floor(Math.random()*3);
     switch(choice){
@@ -45,32 +53,105 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-function game(){
-    let playerCount = 0;
-    let computerCount = 0;
-    for(let i=0; i < 5; i++){
-        let playerSelection = prompt("Rock, Paper or Scissors?", "");
-        
-        while(playerSelection === null || playerSelection === ""){
-            playerSelection = prompt("I'm sorry. Choice cannot be empty!", "");
-        }
-        playerSelection = playerSelection.toLowerCase();
-        while(playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors"){
-            playerSelection = prompt("You have to choose Rock, Paper or Scissors!", "");
-        }
-        let computerSelection = computerPlay();
-        let roundResult = Number(playRound(playerSelection, computerSelection));
-        if( roundResult === 0){
-            console.log(`It's a tie! \nThe score is ${playerCount} - ${computerCount}`);
-        }
-        else if(roundResult === 1){
-            playerCount++;
-            console.log(`You win!! \nThe score is ${playerCount} - ${computerCount}`);
-        }
-        else{
-            computerCount++;
-            console.log(`You lose! \nThe score is ${playerCount} - ${computerCount}`);
-        }
+function updateResult(playerChoice, computerChoice){
+    let result = Number(playRound(playerChoice, computerChoice));
+
+    if( result === 0){
+        // TIE
+        resultChoices.textContent = `${playerChoice} - ${computerChoice}`;
+        resultContainer.appendChild(resultChoices);
+
+        resultText.textContent = "It's a tie!";
+        resultContainer.appendChild(resultText);
+
+        resultScore.textContent = `Player: ${playerScore}   Computer: ${computerScore}`;
+        resultContainer.appendChild(resultScore);
+    }
+    else if(result === 1){
+        // PLAYER WON
+        playerScore++;
+        resultChoices.textContent = `${playerChoice} - ${computerChoice}`;
+        resultContainer.appendChild(resultChoices);
+
+        resultText.textContent = "You won!";
+        resultContainer.appendChild(resultText);
+
+        resultScore.textContent = `Player: ${playerScore}   Computer: ${computerScore}`;
+        resultContainer.appendChild(resultScore);
+    }
+    else{
+        // COMPUTER WON
+        computerScore++;
+        resultChoices.textContent = `${playerChoice} - ${computerChoice}`;
+        resultContainer.appendChild(resultChoices);
+
+        resultText.textContent = "You lost!";
+        resultContainer.appendChild(resultText);
+
+        resultScore.textContent = `Player: ${playerScore}   Computer: ${computerScore}`;
+        resultContainer.appendChild(resultScore);
     }
 }
-game();
+
+function checkWinner(){
+    if(playerScore === 5){
+        resultEnd.textContent = "Congratulations!!! You beat the computer!";
+        resetScore();
+    }
+    else if(computerScore === 5){
+        resultEnd.textContent = "You lost! The computer won this time!";
+        resetScore();
+    }
+    resultContainer.appendChild(resultEnd);
+}
+
+function resetScore(){
+    playerScore = 0;
+    computerScore = 0;
+    
+}
+const rockBTN = document.querySelector('#rock-button');
+rockBTN.addEventListener('click', ()=>{
+    updateResult('rock', computerPlay());
+    checkWinner();
+});
+
+const paperBTN = document.querySelector('#paper-button');
+paperBTN.addEventListener('click', ()=>{
+    updateResult('paper', computerPlay());
+    checkWinner();
+});
+
+const scissorsBTN = document.querySelector('#scissors-button');
+scissorsBTN.addEventListener('click', ()=>{
+    updateResult('scissors', computerPlay());
+    checkWinner();
+});
+
+const resetBTN = document.querySelector('#reset-button');
+resetBTN.addEventListener('click', () =>{
+    resetScore();
+    resultChoices.textContent ="";
+    resultEnd.textContent = "";
+    resultText.textContent = "Score was reset!";
+    resultScore.textContent = "Score is now:  Player: 0 Computer: 0"
+})
+
+
+
+
+// function game(){
+//         while(playerSelection === null || playerSelection === ""){
+//             playerSelection = prompt("I'm sorry. Choice cannot be empty!", "");
+//         }
+//         playerSelection = playerSelection.toLowerCase();
+//         while(playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors"){
+//             playerSelection = prompt("You have to choose Rock, Paper or Scissors!", "");
+//         }
+//         let computerSelection = computerPlay();
+//         let roundResult = Number(playRound(playerSelection, computerSelection));
+        
+// }
+
+
+
